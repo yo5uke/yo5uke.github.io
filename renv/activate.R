ca200873c215692f4c5493eef351869e657ddd57
yo5uk <yosuke.abe0507@gmail.com>
2026-03-20 13:23:05 +0900

    Update renv version to 1.1.8 and remove unnecessary metadata

--- renv/activate.R
+++ renv/activate.R
@@ -2,8 +2,8 @@
 local({
 
   # the requested version of renv
-  version <- "1.1.7"
-  attr(version, "md5") <- "dd5d60f155dadff4c88c2fc6680504b4"
+  version <- "1.1.8"
+  attr(version, "md5") <- NULL
   attr(version, "sha") <- NULL
 
   # the project directory
